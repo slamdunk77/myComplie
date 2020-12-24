35e39b75299968105b6f2f535b7ab963af61a909
slamdunk77 <62142444+slamdunk77@users.noreply.github.com>
2020-12-24 23:51:58 +0800

    Update Test.java

--- Test.java
+++ Test.java
@@ -35,26 +35,10 @@ public class Test {
 //                token.nextToken();
                 // 将识别的每一个token放入一个tokenlist中
                 tokenList.add(token.nextToken());
-                System.out.println(tokenList.get(tokenList.size()-1).getValue());
                 if(it.isEOF())
                     break;
             }
             Analyser.analyseProgram();
-            System.out.println(AnalyserTable.getGlobalDefList().size());
-            for (GlobalDef global : AnalyserTable.getGlobalDefList()) {
-                System.out.println(global);
-            }
-            System.out.println("-----------------------------function");
-            System.out.println(Analyser.getStartFunction());
-            for (FunctionDef functionDef : AnalyserTable.getFunctionDefList()) {
-                System.out.println(functionDef);
-            }
-            for(int i=0;i<AnalyserTable.getInstructionList().size();i++){
-                System.out.print(AnalyserTable.getInstructionList().get(i).getInstr() + " ");
-                System.out.println(AnalyserTable.getInstructionList().get(i).getInstrId());
-
-            }
-
             Binary binary = new Binary(AnalyserTable.getGlobalDefList(), Analyser.getStartFunction(), AnalyserTable.getFunctionDefList());
             DataOutputStream out = new DataOutputStream(new FileOutputStream(new File(args[1])));
             ArrayList<Byte> bytes = binary.generate();
